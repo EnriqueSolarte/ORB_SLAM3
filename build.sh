3b57751883dd57862b721968a4e125e65b7fa84f
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-04-21 15:55:55 +0800

    Add pangoling, fix C++14 and fix ‘operator/’ error

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
+echo "Install Pangolin"
+
+cd /dpds/ && \
+git clone https://github.com/stevenlovegrove/Pangolin.git Pangolin && \
+cd /dpds/Pangolin/ && \
+mkdir build && \
+cd build/ && \
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-DCPP11_NO_BOOST=1 \
+/dpds/Pangolin/ && \
+make -j4 && \
+make install
+
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
@@ -27,5 +40,10 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
+
+# Fix issue of compiler
+# https://github.com/UZ-SLAMLab/ORB_SLAM3/issues/387#issuecomment-958568700
+sed -i 's/++11/++14/g' CMakeLists.txt
+
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
